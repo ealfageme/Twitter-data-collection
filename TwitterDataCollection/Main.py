19eac43c3a9fcb1bc3f171e78281beafddac06a7
ealfageme <e.alfageme@alumnos.urjc.es>
2017-09-26 20:25:48 +0200

    delete database optional

--- TwitterDataCollection/Main.py
+++ TwitterDataCollection/Main.py
@@ -3,22 +3,33 @@ import json
 from py2neo import Graph
 from py2neo.ogm import Property, GraphObject, RelatedTo
 
-# graph instance
-graph = Graph(password='password')
-tx = graph.begin()
+
 # Twitter keys
 consumer_key = '5Xyt2JIpgCWsSboL4jqITlZCb'
 consumer_secret = 'QdgAqnCTeChFurGOyPcGowJAZw5m6eQ14gyZpeaRFw3mQWpoEF'
 access_token = '1924437534-ZnmL5AadDja6bFBkTCfczZLIfOvVbl6iil1ieqw'
 access_token_secret = 'tw0PzDpTsSIbKZuGOxPqxNdCIOsxblUWfB1hCiSN8DAEi'
 
+# graph instance
+graph = Graph(password='password')
+tx = graph.begin()
 
 # Class node
 class Tweet(GraphObject):
     __primarykey__ = "nick"
     text = Property()
     nick = Property()
-     # written_by = RelatedTo(Author)
+    # associate_with = RelatedTo(Hastag)
+    # written_by = RelatedTo(Author)
+
+
+class Author(GraphObject):
+    __primarykey__ = "nick"
+    nick = Property()
+
+class Hastag(GraphObject):
+    __primarykey__ = "name"
+    name = Property()
 
 
 
@@ -44,8 +55,15 @@ class StdOutListener(tweepy.StreamListener):
         print status
 
 if __name__ == '__main__':
-    # Example of object Tweet
 
+
+    result = raw_input("Do you want delete the last search? (Y/N)? :")
+
+    if (result == "Y") or (result == "y"):
+        graph.delete_all()
+        print 'The last search has been deleted'
+    else:
+        print 'The last searches are in database'
 
     # Main
     l = StdOutListener()
@@ -56,6 +74,11 @@ if __name__ == '__main__':
     name = raw_input("Hastag to search:")
     hastag = "#" + name
     print "Showing all new tweets for ", hastag
+
+    # Create de node with the hastag
+    has = Hastag()
+    has.name = hastag
+    graph.create(has)
 
     stream = tweepy.Stream(auth, l)
     stream.filter(track=[hastag])
