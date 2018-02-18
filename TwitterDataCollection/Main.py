19419351f41f6f631ee5537070e25d4585451f6d
ealfageme <e.alfageme@alumnos.urjc.es>
2018-02-18 21:39:27 +0100

    update project structure

--- TwitterDataCollection/Main.py
+++ TwitterDataCollection/Main.py
@@ -1,7 +1,9 @@
 import tweepy
+import sys
+import signal
 import json
 from py2neo import Graph
-from py2neo.ogm import Property, GraphObject, RelatedTo, RelatedFrom
+from py2neo.ogm import Property, GraphObject
 
 # Twitter keys
 consumer_key = '5Xyt2JIpgCWsSboL4jqITlZCb'
@@ -12,7 +14,10 @@ access_token_secret = 'tw0PzDpTsSIbKZuGOxPqxNdCIOsxblUWfB1hCiSN8DAEi'
 # graph instance
 graph = Graph(password='password')
 tx = graph.begin()
-
+# Mode debug don't necessary to introduce the hastag and print the search
+debug = True
+list_user = []
+limit = 0
 
 
 class Account(GraphObject):
@@ -20,58 +25,95 @@ class Account(GraphObject):
     name = Property()
     username = Property()
     tweet = Property()
-    date = Property()
-    time = Property()
-    urlTweet = Property()
-    photo = Property()
+    description = Property()
+    id_twitter = Property()
+    url = Property()
+    verified = Property()
+    location = Property()
+    profile_image_url = Property()
+    followers = Property()
 
 
 # This is the listener, resposible for receiving data
 class StdOutListener(tweepy.StreamListener):
     def on_data(self, data):
-        # Twitter returns data in JSON format - we need to decode it first
-        decoded = json.loads(data)
+        if len(list_user) < limit:
+            # Twitter returns data in JSON format - we need to decode it first
+            decoded = json.loads(data)
 
-        accountobject = Account()
-        accountobject.username = decoded['user']['screen_name']
-        accountobject.tweet = decoded['text'].encode('ascii', 'ignore')
-        accountobject.name = ""
-        accountobject.date = ""
-        accountobject.time = ""
-        accountobject.urlTweet = ""
-        accountobject.photo = ""
+            accountobject = Account()
+            accountobject.username = decoded['user']['screen_name']
+            accountobject.tweet = decoded['text'].encode('ascii', 'ignore')
+            accountobject.name = decoded['user']['name']
+            accountobject.description = decoded['user']['description']
+            accountobject.location = decoded['user']['location']
+            accountobject.id_twitter = decoded['user']['id']
+            accountobject.profile_image_url = decoded['user']['profile_image_url']
+            accountobject.verified = decoded['user']['verified']
+            accountobject.url = decoded['user']['url']
+            accountobject.followers = decoded['user']['followers_count']
 
-        graph.create(accountobject)
+            list_user.append = accountobject
+            graph.create(accountobject)
 
-        # Also, we convert UTF-8 to ASCII ignoring all bad characters sent by users
-        print '@%s: %s' % (decoded['user']['screen_name'], decoded['text'].encode('ascii', 'ignore'))
-        print ''
-        return True
+            if not debug:
+                print "----------------------------------------------------"
+                print "username is :" + accountobject.username
+                print "tweet is    :" + accountobject.tweet
+                print "name is     :" + accountobject.name
+            return True
+        else:
+        # Stop the search
+            pass
 
     def on_error(self, status):
         print status
 
+def be_follow():
+    copy_list = list_user
+    for user in list_user:
+        for user2 in copy_list:
+            if check_following(user,user2):
+                create_relationship(user,user2)
+
+
+def check_following(user, user2):
+    # Check if user user follow to user2
+    return True
+
+def create_relationship(user,user2):
+    # Create the relationship between the node user and the node user2
+    return True
+
+
+def signal_handler(signal, frame):
+    print("You pressed Ctrl+C")
+    print("Stop th application")
+    sys.exit(0)
+
 
 if __name__ == '__main__':
 
-    result = raw_input("Do you want to delete the last search? (Y/N)? :")
-
-    if (result == "Y") or (result == "y"):
-        graph.delete_all()
-        print 'The last search has been deleted'
-    else:
-        print 'The last searches are in database'
-
+    graph.delete_all()
+    print 'The last search has been deleted'
+    signal.signal(signal.SIGINT, signal_handler)
     # Main
     l = StdOutListener()
     auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
     auth.set_access_token(access_token, access_token_secret)
 
-    name = raw_input("Hastag to search:")
-    toSearch = "#" + name
-    print "Showing all new tweets for ", toSearch
-
-
+    if not debug:
+        limit = raw_input("The limit of search:")
+        name = raw_input("Hastag to search:")
+        toSearch = "#" + name
+        print "Showing all new tweets for ", toSearch
+    else:
+        limit = 100
+        toSearch = "#FelizLunes"
 
     stream = tweepy.Stream(auth, l)
     stream.filter(track=[toSearch])
+
+    be_follow()
+
+
